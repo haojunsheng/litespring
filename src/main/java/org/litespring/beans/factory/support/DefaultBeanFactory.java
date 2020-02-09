8a15f591d2e254a0d8501ba25b2c60f068727819
anapodton <1324722673@qq.com>
2020-02-09 17:08:14 +0800

    实现setter注入

--- src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -1,5 +1,6 @@
 package org.litespring.beans.factory.support;
 
+import org.litespring.beans.PropertyValue;
 import org.litespring.beans.factory.BeanCreationException;
 import org.litespring.beans.factory.BeanDefinitionStoreException;
 import org.litespring.beans.factory.BeanFactory;
@@ -10,14 +11,19 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.litespring.beans.factory.config.ConfigurableBeanFactory;
 import org.litespring.util.ClassUtils;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 // BeanFactory的默认实现
-public class DefaultBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory,BeanDefinitionRegistry {
+public class DefaultBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory, BeanDefinitionRegistry {
     // beanDefinitionMap 存放beanID 和 BeanDefinition的映射
     private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>(64);
     private ClassLoader beanClassLoader;
@@ -27,17 +33,17 @@ public class DefaultBeanFactory extends DefaultSingletonBeanRegistry implements 
 
     // 根据beanID获取BeanDefinition
     public BeanDefinition getBeanDefinition(String beanID) {
-        return  this.beanDefinitionMap.get(beanID);
+        return this.beanDefinitionMap.get(beanID);
     }
 
     public void registerBeanDefinition(String beanID, BeanDefinition bd) {
-        this.beanDefinitionMap.put(beanID,bd);
+        this.beanDefinitionMap.put(beanID, bd);
     }
 
     // 根据beanID，通过反射的形式获取类的实例
     public Object getBean(String beanID) {
         BeanDefinition bd = this.getBeanDefinition(beanID);
-        if(bd == null){
+        if (bd == null) {
             return null;
         }
         if (bd.isSingleton()) {
@@ -52,13 +58,54 @@ public class DefaultBeanFactory extends DefaultSingletonBeanRegistry implements 
     }
 
     private Object createBean(BeanDefinition bd) {
+        //创建实例
+        Object bean = instantiateBean(bd);
+        //设置属性
+        populateBean(bd, bean);
+        return bean;
+    }
+
+    private Object instantiateBean(BeanDefinition bd) {
         ClassLoader cl = this.getBeanClassLoader();
         String beanClassName = bd.getBeanClassName();
         try {
             Class<?> clz = cl.loadClass(beanClassName);
             return clz.newInstance();
         } catch (Exception e) {
-            throw new BeanCreationException("create bean for "+ beanClassName +" failed",e);
+            throw new BeanCreationException("create bean for " + beanClassName + " failed", e);
+        }
+    }
+
+    protected void populateBean(BeanDefinition bd, Object bean) {
+        // pvs 是在xml文件中配置的property
+        // pds 是PetStoreService中的定义
+        // 所以要把pvs和pds进行遍历，(双重循环), 符合要求，则使用set方法进行注入
+        List<PropertyValue> pvs = bd.getPropertyValues();
+
+        if (pvs == null || pvs.isEmpty()) {
+            return;
+        }
+
+        BeanDefinitionValueResolver valueResolver = new BeanDefinitionValueResolver(this);
+
+        try {
+            for (PropertyValue pv : pvs) {
+                String propertyName = pv.getName();
+                Object originalValue = pv.getValue();
+                Object resolvedValue = valueResolver.resolveValueIfNecessary(originalValue);
+
+                BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+
+                PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+                for (PropertyDescriptor pd : pds) {
+                    if (pd.getName().equals(propertyName)) {
+                        pd.getWriteMethod().invoke(bean, resolvedValue);
+                        break;
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new BeanCreationException("Failed to obtain BeanInfo for class [" + bd.getBeanClassName() + "]", ex);
         }
     }
 
--- src/test/java/org/litespring/test/v2/ApplicationContextTestV2.java
+++ src/test/java/org/litespring/test/v2/ApplicationContextTestV2.java
@@ -1,9 +1,13 @@
 package org.litespring.test.v2;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.litespring.context.ApplicationContext;
 import org.litespring.context.support.ClassPathXmlApplicationContext;
+import org.litespring.dao.v2.AccountDao;
+import org.litespring.dao.v2.ItemDao;
 import org.litespring.service.v2.PetStoreService;
 
 public class ApplicationContextTestV2 {
@@ -14,5 +18,9 @@ public class ApplicationContextTestV2 {
 
         assertNotNull(petStore.getAccountDao());
         assertNotNull(petStore.getItemDao());
+        assertTrue(petStore.getAccountDao() instanceof AccountDao);
+        assertTrue(petStore.getItemDao() instanceof ItemDao);
+
+        assertEquals("hjs",petStore.getOwner());
     }
 }
